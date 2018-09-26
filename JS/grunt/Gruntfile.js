8f4f816c965bb8eb76f2b4db6a7d111db29f146d
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-09-26 14:18:07 +0200

    added grunt

---
+++ JS/grunt/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        name: 'class',
+        watch: {
+            files: ["*.js", "*.css"],
+            task: ["updated"]
+        },
+        uglify: {
+            build: {
+                src: ['index.js', 'logger.js' ],
+                dest: 'dist/bundle.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', () => {
+        grunt.log.writeln('Its been updated again');
+    });
+
+    grunt.registerTask('updated', () => {
+        grunt.log.writeln('Hello, ${grunt.config.get(name)}');
+    });
+// hello
+    
+};

c76a68238f130388bd2551dc1b61b91733471f35
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-09-28 08:07:15 +0200

    refactored with arrow function

--- JS/react/gamie/src/App.js
+++ JS/react/gamie/src/App.js
@@ -5,17 +5,62 @@ import Gamie from './components/Gamie'
 
 
 class App extends Component {
+  // constructor() {
+  //   super();
+  //   this.toggleActivation = this.toggleActivation.bind(this);
+  //   this.handleChange = this.handleChange.bind(this);
+  // }
+  //destructuring var activated and name into one object state
+          state = {
+            activated: false,
+            name : ''
+          };
+
+  toggleActivation = () => {
+    //assigning activated to the state object same way you assign a class to a variable
+    const {activated} = this.state
+
+    // if (activated) {
+    //   this.setState({ activated: !activated });
+    // } else {
+    //   this.setState({ activated: !activated});
+    // }
+    //using template literals to write an if statement
+    //check if activated is true
+    activated
+    //set state.activated as false
+    ? this.setState({activated: false})
+    //else set state.activated as true
+    : this.setState({activated: true})
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({ name: e.target.value });
+  }
   render() {
+    const {activated, name} = this.state
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome{
+            name.length
+            ? `, ${name}`
+            : ""
+            }</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Gamie />
+
+        <input type="text" onChange={this.handleChange} placeholder="Your name" />
+        <p></p>
+        <div>
+          <button onClick={this.toggleActivation}>Toggle Gamie Code</button>
+          <p></p>
+          {activated && name.length 
+            ? <Gamie  name={name} /> 
+            : <div>Gamie Code: Deactivated</div>
+          }
+        </div>
         
       </div>
     );
--- JS/react/gamie/src/components/Gamie.js
+++ JS/react/gamie/src/components/Gamie.js
@@ -3,26 +3,32 @@ import Mousetrap from 'mousetrap';
 
 
 class Gamie extends Component {
-  constructor() {
-      super();
-  }
+//   constructor(props) {
+//       super(props);
+//       this.popUp.bind(this);
+//   }
 
-  componentDidMount() {
+  componentDidMount = () => {
       Mousetrap.bind(['1 2 3 4', 'up up down down left right left right b a'], this.popUp);
   }
 
-  componentUnmount() {
+  componentUnmount = () => {
       Mousetrap.unbind(['1 2 3 4', 'up up down down left right left right b a']);
   }
 
-  popUp() {
-      alert('The game code worked')
+  popUp = () => {
+      const {name} = this.props
+      alert(`You entered the following${
+          name && name.length 
+          ? `, ${name}!` 
+          : `!`
+        }`);
   }
+  
   render() {
+      const {name} = this.props
     return (
-
-        <div>Hello this is Gamie</div>
-      
+        <div>Hello, {name} this is Gamie</div>
     );
   }
 }

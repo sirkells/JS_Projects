630ad6e86a38ebe95dd26f0faca00f3f12fc237e
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-10-10 14:13:20 +0200

    completed the attack function

--- JS/vue/3. First_project/app.js
+++ JS/vue/3. First_project/app.js
@@ -4,6 +4,54 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameRunning: false
+    },
+    methods:{
+        gameStart: function () {
+            this.gameRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function () {
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if (this.whoWon()) {
+                return;
+            };
+
+            this.playerHealth -= this.calculateDamage(5, 10);
+            this.whoWon();
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
+        },
+        calculateDamage: function(min, max) {
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+            return damage
+        },
+        whoWon: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You Won, Start New Game?')) {
+                    this.gameStart();
+                } else {
+                    this.gameRunning = false;
+                }
+                return;
+            }
+            else if (this.playerHealth <= 0) {
+                if (confirm('You Lost, Start New Game?')) {
+                    this.gameStart();
+                } else {
+                    this.gameRunning = false;
+                }
+                return;
+            }
+        }
+
     }
 
 });
